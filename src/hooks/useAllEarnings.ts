ac2ca765cf452318d69a4188cd1cc232b63d7770
Koolbeean <bowmanng@hotmail.com>
2021-05-20 03:02:01 +0800

    Update useAllEarnings.ts

--- src/hooks/useAllEarnings.ts
+++ src/hooks/useAllEarnings.ts
@@ -15,7 +15,7 @@ const useAllEarnings = () => {
     const fetchAllBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingFfd',
+        name: 'pendingSPCO',
         params: [farm.pid, account],
       }))
 
